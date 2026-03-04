2147c4097842efa77f7c09bf8817b39f4a725165
Cyan <3085650075@qq.com>
2026-03-04 16:50:44 +0800

    feat: add interaction

--- exercise/exer2.cpp
+++ exercise/exer2.cpp
@@ -6,14 +6,19 @@
 int main(void){
 
 	int number;
+	int limit;
 	int i; 
 	int flag; // defalt to true?
-	
+	printf("please key in the ranging number from 1 you want to search prime numer.\n");
+	scanf(" %d",&limit);
 	int count = 0;
 	// obviously, 0 and 1 is not the prime number.
-	number = 2;
-	
-	while ( number<100){
+	if (limit < 2){
+		printf("\nThe range is invalid!!!\n");
+		}else{
+			number = 2;
+		while ( number<limit){
+		
 		i = 2;
 		flag = 1;
 		while ( i<= number-1){
@@ -28,6 +33,7 @@ int main(void){
 		} number++;
 	}
 	printf("After counting, total number of the prime number is %d",count);
+	}
 	
 	return 0;
 }
