478ec29e185d44a73ec87e2bf13f82ab96df292b
Cyan <3085650075@qq.com>
2026-03-18 20:06:25 +0800

    I don't what I need to write for this commit

--- test/bubble_sort.c
+++ test/bubble_sort.c
@@ -2,26 +2,33 @@
 
 // if I don't use the int *a and int *b, so what would happen? 
 // the function only swap the formal parameter 
-// so you need to input the pointer
+// so you need to input the pointer or address of the variable
 
+/*
 void swap(int *a, int *b){
 	int t = *a;
 	*a = *b;
 	*b = t;
 }
+*/
 
 /*
 int len(int arr[]){
 
 	size_t length = sizeof(arr)/ sizeof(arr[0]);
-	// the array input in the function, the array degenerate into the the pointer
+	
+	// The array input in the function, the array degenerate into the the pointer
+	
 	return length;
 }
 */
 
+/*
+
 void bubbleSort(int arr[], int n){
-			
+	// why transfer the arr[] into this function?			
 	for ( int i = 0; i < n-1; i++){
+		// why the circle only < n-1
 		for (int j = 0; j < n -i -1; j++){
 			 if ( arr[j] < arr[j+1]){
 			 	swap(&arr[j] , &arr[j+1]);
@@ -29,6 +36,9 @@ void bubbleSort(int arr[], int n){
 		}
 	}
 }
+*/
+
+/*
 
 int main(void){
 	int a = 32;
@@ -50,7 +60,61 @@ int main(void){
     return 0;
 }
 
+*/	
 	
+// This is my second time to write the bubble sort algorithm again
+
+void swap(int *a, int *b ){
+	int t = *a; // dereference of the variable of a
+	*a = *b; // the value of a now is store in the t, so you can give the value of b to the a
+	*b = t; // then give the original value of the a to the b
+}
+
+void bubble_sort(int arr[], int n){ // we should get the arr name and the arr.length
+	for ( int i = 0; i < n-1; i++){
+	//  For example, you have three elements should be sorted
+	// if the array is {3, 2, 1}, you have swap by two steps only 
+		// so there is `n-1` steps to sorted the array have `n` elements
+		for ( int j =0; j < n-1-i; j ++){
+			// delete the `i` number of the already sorted elements
+			if ( arr[j] > arr[j+1]){
+				swap(&arr[j], &arr[j+1]);
+			}
+		}
+	}
+}
+
+int main(void){
+	// test the function of swap 
+	int a = 114;
+	int b = 514;
 	
+	printf(" This is a: %d .\n This is b: %d . \n", a , b);				 
+	swap(&a, &b);
+	printf(" This new a: %d, b: %d\n",a, b);
+
+	int arr[] = {32, 46, 1122, 77, 2, 0, 31, 88, 321, 32, 41, 99, 23266};
 	
+	/*
+	int n = sizeof(arr[])/sizeof(arr[0]);
+	*/
+	int n = sizeof(arr)/ sizeof(arr[0]);
+	// why is transfer the arr instead of the arr[]
+	// what is the difference between them 
+	// why Cannot design a function to calculate the length of array
 	
+	printf("The original array is\n"); 
+	for ( int i = 0; i< n; i++){
+		printf( "%d , ", arr[i]);
+	}
+	bubble_sort(arr, n);
+	// why only transfe the name of array?
+
+	printf("\n");
+	printf("The sorted array is\n");
+	for ( int i = 0; i< n; i++){
+		printf("%d , ", arr[i]);
+	}
+	 return 0;
+}
+		
---
+++ test/quick_sort.c
@@ -0,0 +1,148 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+// Define the stack struct
+typedef struct{
+	int low;
+	int high;
+}StackItem;
+
+// How can I print the `typedef` struction into the screen?
+
+StackItem stack[100];
+// What this block means?
+// The StackItem include two int,
+// but you define a array?
+ 
+int top = -1;
+// Is this a sentinel?
+
+void push(int low, int high){
+	stack[++top].low = low;
+	// What is this operation meaning?
+	stack[top].high = high;
+	// Push the elements into the stack?
+}
+
+StackItem pop(){
+	return stack[top--];
+}
+// Also like the code of ln19, what is this operation mean?
+
+int isEmpty(){
+	return top == -1;
+}
+ 
+void swap(int *a, int *b){
+	
+	printf("This step I will swap the %d and %d\n", *a, *b);
+	printf("Before the step, `a` is the first number %d\n",*a);
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	printf("Now a is the number %d\n", *a);
+}
+ 
+int partition( int arr[], int low, int high){
+	int pivot = arr[high];
+	// Why the annotation said that
+	// this code choose the last elements
+	// As the pivot?
+	printf("Now the `arr[high]` is %d\n",arr[high]);
+	
+	int i = low - 1;
+	printf("Now the `i` of partition is %d\n", i); 
+	
+	for (int j = low; j < high; j++){
+		if (arr[j] <= pivot){
+			i++;
+			swap(&arr[i], &arr[high]);
+		}
+	}
+	swap(&arr[i+1], &arr[high]);
+	// Why swap the high in the end of partition?
+	return i+1;
+}
+
+void quickSortIterative(int arr[], int low, int high){
+	push(low,high);
+	int step = 1;
+	
+	while( !isEmpty()){
+		StackItem current = pop();
+		low = current.low;
+		high = current.high;
+		
+		if (low < high){
+			printf("\n===The %d.th step ===\n",step++);
+			// What is this fucking operation?
+			// Why the self-add can use in the printf?
+			printf("Process the child-array[%d,%d]:\n",low ,high);
+			for (int i = low; i<=high; i++){
+				printf("| %d | ", arr[i]);
+			}
+			printf("\n");
+			
+			int pi = partition(arr, low, high);
+			
+			printf("After the partitioned, Array belike...\n");
+			for (int i =low; i<=high; i++){
+				printf("| %d | ", arr[i]);
+			}
+			
+			printf("\n Pivot Position: %d",pi);
+			printf("\n Pivot Value: %d", arr[pi]);
+			
+			// The annotation said that
+			// This operation below would
+			// push the left child and right child into stack
+			
+			push(pi+1, high); // Right child array
+			push(low, pi-1); // Left child array
+		}
+	}
+}
+
+int main(void){
+	int array[] = {38, 27, 43, 3, 9, 82, 10};
+	int n = sizeof(array)/ sizeof(array[0]);
+	quickSortIterative(array, 0, n-1);
+
+	printf("\nThe result of Sort:\n");
+	for (int i =0; i < n; i++){
+		printf("| %d | ", array[i]);
+	}
+
+	return 0;
+}
+
+      
+/**
+void swap(int *a, int *b){
+// get the address of a and b;
+// give the number which the address point at  to the t which means temp;
+// then give b to a and give t to b;	
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int main(){
+	int arr[] ={21, 100, 3, 50, 1};
+	for (int i = 0; i < 5; i++){
+		printf("%d ,",arr[i]);
+		if (i == 4) printf("END...\nThis is original array \n");
+	}
+	
+	int *pivot = &arr[0];
+	int *low = &arr[0];
+	int *high = &arr[sizeof(arr)/sizeof(arr[0]) - 1];
+	// why `sizeof(arr)` can calculate the length of array?
+	// how does the funtion `sizeof` work?
+	printf("%p \n",high);
+	printf("%p \n",(void*)high);
+	printf("%d \n",*high);
+	
+	
+}
+*/
